9b7b35c908e1f6f97e50ec71950f989c8568f19a
Roland Angerer <r.angerer@yelsterdigital.com>
2016-03-31 08:42:04 +0200

    initial commit

---
+++ lcd.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def lcd():
+    pass
+
+if __name__ == "__main__":
+    import nose
+    nose.main()
---
+++ test_lcd.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from nose.tools import *
+
+from lcd import lcd
+
+def test_lcd():
+    pass
